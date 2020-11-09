1a9fade46dee8abc287aaa4b11e6476f5431e493
Adilio Gobira <adilio_gobira@hotmail.com>
2020-11-09 08:40:01 -0300

    Update Produto.php

--- src/Engine/Produto.php
+++ src/Engine/Produto.php
@@ -16,7 +16,7 @@ class Produto extends LifeAppsConnector
      */
     public function getSingleProduct($idproduto)
     {
-        $this->endPoint = "/v2/app/" . self::LIFEAPPS_TOKEN_FORNECEDOR . "/fornecedor/" . self::LIFEAPPS_TOKEN_FORNECEDOR . "/produto/{$idproduto}";
+        $this->endPoint = "/v2/app/" . $this->tokenFornecedor . "/fornecedor/" . $this->tokenFornecedor . "/produto/{$idproduto}";
         $this->get();
         return $this->callback;
     }
@@ -27,7 +27,7 @@ class Produto extends LifeAppsConnector
     public function getMixProduct()
     {
         $idcliente = (session()->has('idcliente') ? session()->get("idcliente") : null);
-        $this->endPoint = "/v1/app/" . $this->token . self::LIFEAPPS_TOKEN_SPLIT . $idcliente . "/listaprodutosf/" . self::LIFEAPPS_TOKEN_FORNECEDOR . "?sk=&page=0&formapagamento=f430d089-93e0-4d2a-bd4b-f7ddca530a7b&ordenacao=Relev%C3%A2ncia%20na%20pesquisaZ&canalVenda=WEB&tagsPesquisa=";
+        $this->endPoint = "/v1/app/" . $this->token . $this->tokenSplit . $idcliente . "/listaprodutosf/" . $this->tokenFornecedor . "?sk=&page=0&formapagamento=f430d089-93e0-4d2a-bd4b-f7ddca530a7b&ordenacao=Relev%C3%A2ncia%20na%20pesquisaZ&canalVenda=WEB&tagsPesquisa=";
 
         $this->get();
         return $this->callback;
@@ -41,7 +41,7 @@ class Produto extends LifeAppsConnector
     public function getMixProductByCat(string $category, $order = "Relev%C3%A2ncia+na+pesquisa", $page = 0)
     {
         $idcliente = (session()->has('idcliente') ? session()->get("idcliente") : null);
-        $this->endPoint = "/v1/app/" . $this->token . self::LIFEAPPS_TOKEN_SPLIT . $idcliente . "/listaprodutosf/" . self::LIFEAPPS_TOKEN_FORNECEDOR . "?categ={$category}&sk=&page={$page}&formapagamento=f430d089-93e0-4d2a-bd4b-f7ddca530a7b&ordenacao={$order}&canalVenda=WEB&tagsPesquisa=";
+        $this->endPoint = "/v1/app/" . $this->token . $this->tokenSplit . $idcliente . "/listaprodutosf/" . $this->tokenFornecedor . "?categ={$category}&sk=&page={$page}&formapagamento=f430d089-93e0-4d2a-bd4b-f7ddca530a7b&ordenacao={$order}&canalVenda=WEB&tagsPesquisa=";
 
         $this->get();
         return $this->callback;
@@ -57,7 +57,7 @@ class Produto extends LifeAppsConnector
     {
         $search = urlencode($search);
         $idcliente = (session()->has('idcliente') ? session()->get("idcliente") : null);
-        $this->endPoint = "/v1/app/" . $this->token . self::LIFEAPPS_TOKEN_SPLIT . $idcliente . "/listaprodutosf/" . self::LIFEAPPS_TOKEN_FORNECEDOR . "?sk={$search}&page={$page}&offset=100&formapagamento=f430d089-93e0-4d2a-bd4b-f7ddca530a7b&ordenacao={$order}&canalVenda=WEB&tagsPesquisa=";
+        $this->endPoint = "/v1/app/" . $this->token . $this->tokenSplit . $idcliente . "/listaprodutosf/" . $this->tokenFornecedor . "?sk={$search}&page={$page}&offset=100&formapagamento=f430d089-93e0-4d2a-bd4b-f7ddca530a7b&ordenacao={$order}&canalVenda=WEB&tagsPesquisa=";
 
         $this->get();
         return $this->callback;
