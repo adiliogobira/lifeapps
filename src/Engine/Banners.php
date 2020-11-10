0ce2dba40a94847d9d2131b6d176c71d108efceb
Adilio Gobira <adilio_gobira@hotmail.com>
2020-11-10 15:31:28 -0300

    Update Banners.php

--- src/Engine/Banners.php
+++ src/Engine/Banners.php
@@ -13,7 +13,7 @@ class Banners extends LifeAppsConnector
      */
     public function getCarrouseu()
     {
-        $this->endPoint = "/v2/app/" . $this->token . "/customizacao/carrossel/list-for-page/" . $this->tokenFornecedor;
+        $this->endPoint = "/v2/app/" . $this->token . "/customizacao/carrossel/list-for-page/" . $this->tokenFornecedor. "?page=page:home%20%20%20%20&device=desktop&idformapagamento=f430d089-93e0-4d2a-bd4b-f7ddca530a7b";
         $this->get();
         return $this->callback;
     }
@@ -23,7 +23,7 @@ class Banners extends LifeAppsConnector
      */
     public function getBanners()
     {
-        $this->endPoint = "/v2/app/" . $this->token . "/customizacao/carrossel/list-for-page/" . $this->tokenFornecedor;
+        $this->endPoint = "/v2/app/" . $this->token . "/customizacao/carrossel/list-for-page/" . $this->tokenFornecedor. "?page=page:home%20%20%20%20&device=desktop&idformapagamento=f430d089-93e0-4d2a-bd4b-f7ddca530a7b";
         $this->get();
         return $this->callback;
     }
