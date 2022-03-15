bfb2b7c2fe8c16cdeec9db949402857252d223a3
Adilio Gobira <adilio_gobira@hotmail.com>
2022-03-15 09:43:27 -0300

    Update lifeapps.php

--- config/lifeapps.php
+++ config/lifeapps.php
@@ -1,6 +1,9 @@
 <?php
 return [
-   
+   /**----------------------------------------------
+    *  Variaveis de ocnexão com a api.
+    *  ----------------------------------------------
+   */
     'LIFE_APPS_FORNECEDOR'  => env('LIFE_APPS_FORNECEDOR', ''),
     'LIFE_APPS_TOKEN_SPLIT' =>  env('LIFE_APPS_TOKEN_SPLIT', ''),
     'LIFE_APPS_URL'         =>  env('LIFE_APPS_URL', '')
